3f6a671bbd9308af1a434124356852c9a3e6f4f1
Milosz Lewandowski <milosz.lewandowski@icloud.com>
2023-03-05 16:40:31 +0100

    dll init

--- public/index.js
+++ public/index.js
@@ -1,2 +1,15 @@
 "use strict";
-console.log('TEST');
+class _Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+        this.prev = null;
+    }
+}
+class DoublyLinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+}
