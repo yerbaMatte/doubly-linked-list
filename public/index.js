bb0d7a5916969a220c4f0ffab5d093d21b27361d
Milosz Lewandowski <milosz.lewandowski@icloud.com>
2023-03-06 13:42:10 +0100

    add private/public access modifiers

--- public/index.js
+++ public/index.js
@@ -12,4 +12,25 @@ class DoublyLinkedList {
         this.tail = null;
         this.length = 0;
     }
+    push(val) {
+        const newNode = new _Node(val);
+        if (this.head === null || this.tail === null) {
+            this.head = newNode;
+            this.tail = newNode;
+        }
+        else {
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
 }
+const newList = new DoublyLinkedList();
+function loggingIdentity(arg) {
+    console.log(arg.length);
+    return arg;
+}
+let myArray;
+let myArray2;
--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,69 @@
-class _Node {
-  val: any;
-  next: node;
-  prev: node;
+class _Node<T> {
+  public val: T;
+  public next: _Node<T> | null;
+  public prev: _Node<T> | null;
 
-  constructor(val: any) {
+  constructor(val: T) {
     this.val = val;
     this.next = null;
     this.prev = null;
   }
 }
 
-type node = null | _Node;
-
-class DoublyLinkedList {
-  head: node;
-  tail: node;
-  length: number;
+class DoublyLinkedList<T> {
+  private head: _Node<T> | null;
+  private tail: _Node<T> | null;
+  private length: number;
 
   constructor() {
     this.head = null;
     this.tail = null;
     this.length = 0;
   }
+
+  public push(val: T): DoublyLinkedList<T> {
+    const newNode = new _Node(val);
+    if (this.head === null || this.tail === null) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    if (this.length === 0) return undefined;
+    const lastNode = this.tail;
+    if (this.length === 1) {
+      this.tail = null;
+      this.head = null;
+    } else {
+      this.tail = lastNode.prev;
+      this.tail.next = null;
+    }
+    this.length--;
+    return lastNode;
+  }
 }
+
+const newList = new DoublyLinkedList();
+
+function loggingIdentity<Type>(arg: Type[]): Type[] {
+  console.log(arg.length);
+  return arg;
+}
+
+let myArray: number[];
+let myArray2: Array<number>;
+
+type JobRun = {
+  job: any;
+  onComplete: (cb: (job: any) => void) => void;
+};
+
+let x: any = 'hello';
+let y: number = x.length;
